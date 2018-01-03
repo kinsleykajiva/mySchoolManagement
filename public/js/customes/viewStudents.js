0e93ade2f245dfbde19ac89af031382e0e4cd038
KINSLEY-KAJIVA\Kajiva Kinsley <kajivakinsley@gmail.com>
2018-01-03 07:12:00 +0300

    done with all the CRUD operation in the database, left with creating modules of the pages

--- public/js/customes/viewStudents.js
+++ public/js/customes/viewStudents.js
@@ -8,7 +8,7 @@ var rowBtns = function(id){
 	return "<center>" +
 	"<button id='"+id+"' onclick='viewRecBtn(this)' class=' btn btn-circle btn-3d  btn-mn btn-primary' data-toggle='tooltip' data-placement='left' title='View Record'>  <span class='fa fa-info-circle'></span> </button>" +
 	"<button id='"+id+"' onclick='editbnt(this)' class=' btn btn-circle btn-3d  btn-mn btn-info' data-toggle='tooltip' data-placement='top' title='Edit'>  <span class='fa fa-pencil-square'></span> </button>" +
-	"<button id='"+id+"' onclick='deletebnt(this)' class='btn btn-circle btn-3d btn-mn btn-danger' data-toggle='tooltip' data-placement='right' title='Delete'>  <span class='fa fa-minus-circle'></span> </button>" +
+	"<button id='"+id+"' onclick='deletebnt(this)' class='btn btn-circle btn-3d btn-mn btn-danger' data-toggle='tooltip' data-placement='bottom' title='Delete'>  <span class='fa fa-minus-circle'></span> </button>" +
 
 	"</center>"};
 
@@ -28,13 +28,12 @@ var tdCheckboxChecked = []; // checked checbox arr
 
 //Select or deselect all checkboxes on main checkbox change
 selectAll.on('click', function() {
-
 	tdCheckbox.prop('checked', this.checked);
 	tdCheckboxChecked = table.find('tbody input:checkbox:checked'); //Collect all checked checkboxes from tbody tag
 });
 //Switch main checkbox state to checked when all checkboxes inside tbody tag is checked
 tdCheckbox.on('change', function() {
-
+alert("asaa");
 	tdCheckboxChecked = table.find('tbody input:checkbox:checked'); //Collect all checked checkboxes from tbody tag
 	//if length of already checked checkboxes inside tbody tag is the same as all tbody checkboxes length, then set property of main checkbox to "true", else set to "false"
 	selectAll.prop('checked', (tdCheckboxChecked.length == tdCheckbox.length));
@@ -42,6 +41,28 @@ tdCheckbox.on('change', function() {
 });
 /*********************************************************************************************/
 
+/*********************************************************************************************/
+/*********************************************************************************************/
+
+/*********************************************************************************************/
+$("#actOnSelectedRows").click(function () {
+	tdCheckboxChecked = table.find('tbody input:checkbox:checked'); //Collect all checked checkboxes from tbody tag
+	
+	selectAll.prop('checked', (tdCheckboxChecked.length == tdCheckbox.length));
+	/*if( tdCheckboxChecked.length == 1 ){
+		deletebnt(tdCheckboxChecked[0]);
+	}else if ( tdCheckboxChecked.length > 1) {
+			for (var i = 0; i < tdCheckboxChecked.length; i++) {
+			console.log(tdCheckboxChecked[i]);
+			alert(tdCheckboxChecked[i])
+
+		}
+	}*/
+	alert("To be done next week")
+	
+	
+});
+/*********************************************************************************************/
 /*********************************************************************************************/
 $("#btnEditNextStudentData").click(function(){
 	var pos = $(window).scrollTop();
@@ -205,8 +226,9 @@ $("#btnEditNextStudentData").click(function(){
 
 /*********************************************************************************************/
 $("#btnEditPrevStudentData").click(function() {
-$("#orJustsaveTxt").show();
-$("#btnEditJustSaveStudentData").show();
+
+	$("#orJustsaveTxt").show();
+	$("#btnEditJustSaveStudentData").show();
 	$("#gardianDetailsSection").hide("slide", {
 		direction: "left"
 	}, 100, function() {
@@ -222,8 +244,6 @@ $("#btnEditJustSaveStudentData").show();
 		$("#btnEditJustSaveStudentData").show();
 
 	});
-
-
 });
 /*********************************************************************************************/
 $("#btnEditSaveStudentData , #btnEditJustSaveStudentData").click(function(argument) {
@@ -391,7 +411,7 @@ function getAllStudents(){
 	if (lastAllResultsMap.size == 0) {
 		$.get("http://localhost:3500/getAllStudentData",{},
 			function(response){
-				if(response  == ''){
+				if(response .length == 0 ){
 					showSpinnerRow(false);
 					showNoRecordRow(true);
 					return;
@@ -417,8 +437,11 @@ function getAllStudents(){
 						item.date_ // at - 15
 
 					]);					
-					let tr = "<tr>" +
-										"<td> <input type='checkbox' name='checkbox1' id='" + item.reg_num + "'  ></td>" +
+					let tr = "<tr>" +  "<td>  <div class='checkbox checkbox-success'>"+
+													" <input type='checkbox' class='styled' name='checkbox1' id='" + item.reg_num + "'  >"+
+													"<label for='" + item.reg_num + "'> </label>"+
+												"</div>"+
+										"</td>" +
 										"<td>" + item.reg_num + "</td>" +
 										"<td>" + item.inputFirstname + "</td>" +
 										"<td>" + item.inputSurname + "</td>" +
@@ -431,13 +454,18 @@ function getAllStudents(){
 								"</tr>";
 					tbody.append(tr);
 				});
+				$("#showingResultsID").text("Showing "+lastAllResultsMap.size +" Record(s) found.")
 				showSpinnerRow(false);
 			});
 	}else{
 		for (const entry of lastAllResultsMap.entries() ) {
 
 				let tr = "<tr>" +
-											"<td> <input type='checkbox' name='checkbox1' id='" + entry[0]  + "'  ></td>" +
+											"<td> <div class='checkbox checkbox-success'>"+
+													" <input class='styled'  type='checkbox' name='checkbox1' id='" + entry[0]  + "'  >"+
+													"<label for='" + entry[0] + "'> .</label>"+
+													"</div>"+
+											"</td>" +
 											"<td>" + entry[0] + "</td>" +
 											"<td>" + entry[1][2] + "</td>" +
 											"<td>" + entry[1][3] + "</td>" +
@@ -448,23 +476,159 @@ function getAllStudents(){
 											"<td> " + rowBtns( entry[0] ) + "</td>" +
 											
 									"</tr>";
-						tbody.append(tr);
+					tbody.append(tr);
 		}
+		$("#showingResultsID").text("Showing "+lastAllResultsMap.size +" Record(s) found.")
 		showSpinnerRow(false);
 	}
+
 
 
 }
 getAllStudents();
 /*********************************************************************************************/
+/*********************************************************************************************/
+function filterByGrade () {
+	var count_grad = 0;
+	let grad_ = $("#searchGradeLevel").val();
+	let tbody = $("#showList");
+	showNoRecordRow(false);
+	tbody.empty();
+	showSpinnerRow(true);
+	
+	if (grad_ == 'null'){		
+		getAllStudents();
+	}else{
 
+		for (const entry of lastAllResultsMap.entries()) {
+			if (entry[1][0] == grad_) {
+				count_grad++;
+				let tr = "<tr>" +
+					"<td> <div class='checkbox checkbox-success'>" +
+					" <input class='styled'  type='checkbox' name='checkbox1' id='" + entry[0] + "'  >" +
+					"<label for='" + entry[0] + "'> .</label>" +
+					"</div>" +
+					"</td>" +
+					"<td>" + entry[0] + "</td>" +
+					"<td>" + entry[1][2] + "</td>" +
+					"<td>" + entry[1][3] + "</td>" +
+					"<td>" + entry[1][4] + "</td>" +
+					"<td>" + entry[1][5] + "</td>" +
+					"<td>" + entry[1][0] + ' ' + entry[1][1] + "</td>" +
+					"<td>" + dateConvertor(entry[1][15]) + "</td>" +
+					"<td> " + rowBtns(entry[0]) + "</td>" +
+
+					"</tr>";
+				tbody.append(tr);
+
+			}
+		}
+		
+		if (count_grad == 0) {
+			
+			showNoRecordRow(true);
+
+		}
+
+		showSpinnerRow(false);
+	}
+	
+}
+/*********************************************************************************************/
+function searchData(){
+	let query = $("#searchtxt").val().trim();
+	let grad_ = $("#searchGradeLevel").val();
+	if (query == '') {
+		showWarningMessage("Nothing to search.", 6000);
+		return;
+	}
+	showNoRecordRow(false);
+	showSpinnerRow(true);
+	$.post(grad_ == 'null' ? "http://localhost:3500/getStudentDataSearch" : "http://localhost:3500/getStudentDataSearchGrade", {
+		query: query,
+		grad_: grad_
+	}, function(response) {
+		let grad_ = $("#searchGradeLevel").val();
+		let tbody = $("#showList");
+		showSpinnerRow(false);
+		tbody.empty();
+		if (response.length == 0) {
+			showNoRecordRow(true);
+		} else {
+
+			response.forEach(function(item) {
+
+				let tr = "<tr>" + "<td>  <div class='checkbox checkbox-success'>" +
+					" <input type='checkbox' class='styled' name='checkbox1' id='" + item.reg_num + "'  >" +
+					"<label for='" + item.reg_num + "'> </label>" +
+					"</div>" +
+					"</td>" +
+					"<td>" + item.reg_num + "</td>" +
+					"<td>" + item.inputFirstname + "</td>" +
+					"<td>" + item.inputSurname + "</td>" +
+					"<td>" + item.inputGender + "</td>" +
+					"<td>" + item.inputDob + "</td>" +
+					"<td>" + item.inputClassLevel + ' ' + item.inputClass + "</td>" +
+					"<td>" + dateConvertor(item.date_) + "</td>" +
+					"<td> " + rowBtns(item.reg_num) + "</td>" +
+
+					"</tr>";
+				tbody.append(tr);
+
+			});
+		}
+	});
+}
 /*********************************************************************************************/
 /*********************************************************************************************/
+/*********************************************************************************************/
+$("#btnreserSearch").click(function () {
+	
+	if($("#searchtxt").val().trim() != '' || $("#searchGradeLevel").val() !='null' ){
+		getAllStudents();
 
+	}
+	$("#searchtxt").val('');
+	$("#searchGradeLevel").val('null');
+});
+/*********************************************************************************************/
+function recordDeleteDismiss()  {
+	$("#DeleteDectionContainer").slideUp("slow");
+	loadingScreenElement("blockTableVieList" ,false,"Locked");
+	diableSearchFunctions(false);
+}
+/*********************************************************************************************/
+function recordDelete () {
+	const id__  = $("#recID").text();
+	
+	loadingScreen(true, "Deleting , Please wait....");
+
+	$.post("http://localhost:3500/getDeleteStudent",
+		{id_:id__},function (response) {
+			loadingScreen(false, "Updating , Please wait....");
+			if(response == 'done'){
+				showSuccessMessage("Deleted Record" , 5000);
+				lastAllResultsMap.delete(id_);
+				getAllStudents();
+				$("#DeleteDectionContainer").slideUp("slow");
+			}else{
+				showErrorMessage("An Error occurred Please try again.");
+			}
+		});
+
+	
+}
 /*********************************************************************************************/
 
 function deletebnt(obj){
-	
+	const id__ = $(obj).attr('id');
+	$("#recID").text(id__);
+	$("html, body").animate({
+			scrollTop: 3
+		}, "fast");
+	$("#DeleteDectionContainer").slideDown("fast");
+	loadingScreenElement("blockTableVieList" ,true,"Locked");
+	diableSearchFunctions(true);
 }
 /*********************************************************************************************/
 function editbnt(obj){
@@ -563,6 +727,14 @@ $("#btnViewInFoBacktoListView").click(function () {
 /*********************************************************************************************/
 /*********************************************************************************************/
 /*********************************************************************************************/
+function diableSearchFunctions (is) {
+	if(is){
+		$('#searchdiv :input , #btnreserSearch').attr('disabled', true).addClass("disabledDiv");
+	}else{
+		 $('#searchdiv :input , #btnreserSearch').removeAttr('disabled').removeClass("disabledDiv");
+	}
+}
+
 /*********************************************************************************************/
 function dateConvertor (zDate) {
 	let  mydate = new Date(zDate);
@@ -601,6 +773,7 @@ function hideValidateError(elementID) {
 /*********************************************************************************************/
 function showSpinnerRow(toShow) {
 	if (toShow) {
+
 		$("#loadingRoweFetch").show();
 	} else {
 		$("#loadingRoweFetch").hide();
